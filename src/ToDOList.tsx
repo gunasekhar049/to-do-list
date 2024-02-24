698af81e07082761492d8f6d20e6f766fcecba6b
Sompalle.sekhar@synoriq.in <GunA@049>
2024-02-25 03:30:32 +0530

    setup for todolist

---
+++ src/ToDOList.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const ToDOList = () => {
+    
+    const [tasks,setTasks] = useState([]);
+    const [newTask,setNewTask] = useState("");
+
+    const handleInoutChange = () => {
+
+    }
+
+    const addTask = () => {
+         
+    }
+
+    const deleteTask = () => {
+
+    }
+
+    const moveTaskUp = () => {
+
+    }
+
+    const moveTaskDown = () => {
+
+    }
+
+
+  return (
+    <div>
+      Hii
+    </div>
+  )
+}
+
+export default ToDOList
